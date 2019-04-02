eb6ddd059733e87178f63e925d32bffc861dafb6
Thomas <thomas.camminady@rwth-aachen.de>
2019-04-02 14:48:32 +0200

    Comply with snake_case and add docstrings.

--- test/test_transformations.py
+++ test/test_transformations.py
@@ -9,7 +9,7 @@ from numpy.linalg import norm
 from sphericalquadpy.tools.transformations import xyz2thetaphi, thetaphi2xyz
 
 
-def test_xyz2thetaphi2xyz():
+def test_xyz2thetaphi_then_thetaphi2xyz():
     """Starting from a point in (x,y,z), we have to return to the same point
     when performing the transformation to (theta,phi) followed by the
     transformation back to (x,y,z)."""
@@ -25,10 +25,11 @@ def test_xyz2thetaphi2xyz():
     assert norm(poles - newpoles) < 1.0e-14
 
 
-def test_thetaphi2xyz2thetaphi():
+def test_thetaphi2xyz_then_xyz2thetaphi():
     """Starting from a point in (theta,phi), we have to return to the same
     point when performing the transformation to (x,y,z) followed by the
     transformation back to (theta,phi)."""
+
     poles = array([[0, 0],
                    [0, pi / 2],
                    [pi / 2, pi / 2],
@@ -40,19 +41,29 @@ def test_thetaphi2xyz2thetaphi():
     assert norm(poles - newpoles) < 1.0e-14
 
 
-def test_xyz2thetaphiDimensionException():
+def test_xyz2thetaphi_dimension_exception():
+    """Test whether we raise an exception for the
+    case when we give an input that has not dimension 3."""
+
     invalidpoint = array([[1, 0, 0, 0]])
     with pytest.raises(Exception):
         _ = xyz2thetaphi(invalidpoint)
 
 
-def test_thetaphi2xyzDimensionException():
+def test_thetaphi2xyz_dimension_exception():
+    """Test whether we raise an exception for the
+    case when we give an input that has not dimension 3."""
+
     invalidpoint = array([[pi, pi, 0]])
     with pytest.raises(Exception):
         _ = thetaphi2xyz(invalidpoint)
 
 
-def test_xyz2thetaphiPointNotOnUnitSphereException():
+def test_xyz2thetaphi_point_not_on_unitsphere_exception():
+    """Test whether we raise an exception for the
+    case when the point does not have norm one and therefore
+    is not part of the unit sphere."""
+
     invalidpoint = array([[1.1, 0, 0, ]])
     with pytest.raises(Exception):
         _ = xyz2thetaphi(invalidpoint)
