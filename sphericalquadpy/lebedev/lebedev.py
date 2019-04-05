1a22bf3f840432e0e4b1f2aca286b0b3b28e4645
Thomas <thomas.camminady@rwth-aachen.de>
2019-04-05 15:42:02 +0200

    Work on lebedev quadrature.

--- sphericalquadpy/lebedev/lebedev.py
+++ sphericalquadpy/lebedev/lebedev.py
@@ -4,65 +4,13 @@ from sphericalquadpy.quadrature.quadrature import Quadrature
 from sphericalquadpy.tools.findnearest import find_nearest
 from sphericalquadpy.lebedev.writtendict import lebedevdictionary
 
-AVAILABLEORDERS = [
-    3,
-    5,
-    7,
-    9,
-    11,
-    13,
-    15,
-    17,
-    19,
-    21,
-    23,
-    25,
-    27,
-    29,
-    31,
-    35,
-    41,
-    47,
-    53,
-    59,
-    65,
-    71,
-    77, 83, 89, 95, 101, 107, 113, 119, 125, 131]
+AVAILABLEORDERS = [3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31, 35,
+                   41, 47, 53, 59, 65, 71, 77, 83, 89, 95, 101, 107, 113, 119,
+                   125, 131]
 
-NUMBERQUADPOINTS = [
-    6,
-    14,
-    26,
-    38,
-    50,
-    74,
-    86,
-    110,
-    146,
-    170,
-    194,
-    230,
-    266,
-    302,
-    350,
-    434,
-    590,
-    770,
-    974,
-    1202,
-    1454,
-    1730,
-    2030,
-    2354,
-    2702,
-    3074,
-    3470,
-    3890,
-    4334,
-    4802,
-    5294,
-    5810,
-]
+NUMBERQUADPOINTS = [6, 14, 26, 38, 50, 74, 86, 110, 146, 170, 194, 230, 266,
+                    302, 350, 434, 590, 770, 974, 1202, 1454, 1730, 2030, 2354,
+                    2702, 3074, 3470, 3890, 4334, 4802, 5294, 5810]
 
 
 # NUMBERQUADPOINTS = zeros(len(AVAILABLEORDERS), dtype=int)
@@ -73,6 +21,12 @@ NUMBERQUADPOINTS = [
 
 class Lebedev(Quadrature):
     """Lebedev Quadrature"""
+
+    def name(self):
+        return "Lebedev Quadrature"
+
+    def getmaximalorder(self):
+        return 131
 
     def computequadpoints(self, order):
         """Quadrature points for Lebedev quadrature. Read from file."""
@@ -85,15 +39,15 @@ class Lebedev(Quadrature):
 
         d = lebedevdictionary()
         xyzw = d[order]
-        return xyzw[:, 0:2]
+        return xyzw[:, 0:3]
 
     def computequadweights(self, order):
         """Quadrature weights for Lebedev quadrature. Read from file."""
         if order not in AVAILABLEORDERS:
             neighbor = find_nearest(AVAILABLEORDERS, order)
             raise ValueError(
-                "Order not available. Next closest would be %i.",
-                AVAILABLEORDERS[neighbor],
+                "Order not available. Next closest would be %i. You chose %i",
+                AVAILABLEORDERS[neighbor], order
             )
 
         d = lebedevdictionary()
@@ -106,4 +60,4 @@ class Lebedev(Quadrature):
     def nqbyorder(self, order):
         """Scaling was derived from files in data/"""
         idx = find_nearest(AVAILABLEORDERS, order)
-        return NUMBERQUADPOINTS[idx]
+        return AVAILABLEORDERS[idx], NUMBERQUADPOINTS[idx]
